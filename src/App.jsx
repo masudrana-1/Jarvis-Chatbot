3e9cc04f3bba5870563c62a0fb5f9eff0e7df0ef
Masud Rana <masudrana58855@gmail.com>
2023-11-17 19:49:18 +0600

    create message

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,27 @@ import '@chatscope/chat-ui-kit-react-styles/dist/default/style.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am Jarvis.",
+      sender: "ChatGPT"
+    }
+  ])
 
   return (
     <div className='App'>
-      
+      <div style={{position: "relative", height: "800px", width: "700px"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {messages.map((message, index) => {
+                return <Message key={index} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder='Type message here' onSend={handleSend}/>
+          </ChatContainer>
+        </MainContainer>
+      </div>
     </div>
   )
 }
